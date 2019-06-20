4099e9752c363f6dca87d39496e8396c867cf3df
Artyom Sovetnikov <elringus@yandex.ru>
2019-06-20 20:49:12 +0300

    add broadcast event command

---
+++ Assets/NaninovelPlayMaker/Runtime/Commands/BroadcastPlayMakerEvent.cs
@@ -0,0 +1,27 @@
+﻿using Naninovel.Commands;
+using System.Threading.Tasks;
+
+namespace Naninovel.PlayMaker
+{
+    /// <summary>
+    /// Broadcasts a global PlayMaker event with the provided name.
+    /// </summary>
+    [CommandAlias("playmaker")]
+    public class BroadcastPlayMakerEvent : Command
+    {
+        /// <summary>
+        /// Name of the event to broadcast.
+        /// </summary>
+        [CommandParameter(NamelessParameterAlias)]
+        public string EventName { get => GetDynamicParameter<string>(null); set => SetDynamicParameter(value); }
+
+        public override Task ExecuteAsync ()
+        {
+            PlayMakerFSM.BroadcastEvent(EventName);
+
+            return Task.CompletedTask;
+        }
+
+        public override Task UndoAsync () => Task.CompletedTask;
+    }
+}
