438bc0abe5dba4d86b884140c681b007fb4423aa
Artyom Sovetnikov <elringus@yandex.ru>
2019-06-23 11:28:13 +0300

    add event router

---
+++ Assets/NaninovelPlayMaker/Runtime/EventRouter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Naninovel.PlayMaker
+{
+    /// <summary>
+    /// Routes (broadcasts) some of the essential Naninovel events to the PlayMaker FSMs.
+    /// </summary>
+    internal static class EventRouter
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Initialize ()
+        {
+            if (Engine.IsInitialized) HandleEngineInitialized();
+            else
+            {
+                Engine.OnInitialized -= HandleEngineInitialized;
+                Engine.OnInitialized += HandleEngineInitialized;
+            }
+        }
+
+        private static void HandleEngineInitialized ()
+        {
+            PlayMakerFSM.BroadcastEvent("Naninovel/Engine/OnInitialized");
+
+            var stateMngr = Engine.GetService<StateManager>();
+            stateMngr.OnGameSaveStarted += _ => PlayMakerFSM.BroadcastEvent("Naninovel/StateManager/OnGameSaveStarted");
+            stateMngr.OnGameSaveFinished += _ => PlayMakerFSM.BroadcastEvent("Naninovel/StateManager/OnGameSaveFinished");
+            stateMngr.OnGameLoadStarted += _ => PlayMakerFSM.BroadcastEvent("Naninovel/StateManager/OnGameLoadStarted");
+            stateMngr.OnGameLoadFinished += _ => PlayMakerFSM.BroadcastEvent("Naninovel/StateManager/OnGameLoadFinished");
+
+            var scriptPlayer = Engine.GetService<ScriptPlayer>();
+            scriptPlayer.OnPlay += () => PlayMakerFSM.BroadcastEvent("Naninovel/ScriptPlayer/OnPlay");
+            scriptPlayer.OnStop += () => PlayMakerFSM.BroadcastEvent("Naninovel/ScriptPlayer/OnStop");
+        }
+    }
+}
