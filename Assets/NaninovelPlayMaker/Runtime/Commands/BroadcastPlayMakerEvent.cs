ed6c6cd7622e199b235ce4faf96dcf1685012c91
Artyom Sovetnikov <elringus@yandex.ru>
2019-06-29 14:59:19 +0300

    add `fsm` and `object` params for broadcast event command

--- Assets/NaninovelPlayMaker/Runtime/Commands/BroadcastPlayMakerEvent.cs
+++ Assets/NaninovelPlayMaker/Runtime/Commands/BroadcastPlayMakerEvent.cs
@@ -1,10 +1,12 @@
 ﻿using Naninovel.Commands;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Naninovel.PlayMaker
 {
     /// <summary>
-    /// Broadcasts a global PlayMaker event with the provided name.
+    /// Broadcasts a PlayMaker event with the provided name.
+    /// When neither `fsm` nor `object` params are specified, the event will be broadcasted globally to all the active FSMs on scene.
     /// </summary>
     [CommandAlias("playmaker")]
     public class BroadcastPlayMakerEvent : Command
@@ -14,10 +16,31 @@ namespace Naninovel.PlayMaker
         /// </summary>
         [CommandParameter(NamelessParameterAlias)]
         public string EventName { get => GetDynamicParameter<string>(null); set => SetDynamicParameter(value); }
+        /// <summary>
+        /// Names of FSMs for which to broadcast the event.
+        /// </summary>
+        [CommandParameter("fsm", true)]
+        public string[] FsmNames { get => GetDynamicParameter<string[]>(null); set => SetDynamicParameter(value); }
+        /// <summary>
+        /// Names of game objects for which to broadcast the event. The objects should have an FSM component attached.
+        /// </summary>
+        [CommandParameter("object", true)]
+        public string[] GameObjectNames { get => GetDynamicParameter<string[]>(null); set => SetDynamicParameter(value); }
 
         public override Task ExecuteAsync ()
         {
-            PlayMakerFSM.BroadcastEvent(EventName);
+            if (FsmNames is null && GameObjectNames is null)
+                PlayMakerFSM.BroadcastEvent(EventName);
+
+            var fsmNames = FsmNames?.ToList();
+            var objectNames = GameObjectNames?.ToList();
+            foreach (var fsm in PlayMakerFSM.FsmList)
+            {
+                if (objectNames != null && !objectNames.Contains(fsm.gameObject.name)) continue;
+                if (fsmNames != null && !fsmNames.Contains(fsm.FsmName)) continue;
+
+                fsm.SendEvent(EventName);
+            }
 
             return Task.CompletedTask;
         }
