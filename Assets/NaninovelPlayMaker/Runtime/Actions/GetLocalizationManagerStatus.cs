73393fd77d605b08273ccfed676e2f2d6c762153
Artyom Sovetnikov <elringus@yandex.ru>
2019-08-16 13:26:31 +0300

    add loc manager status

---
+++ Assets/NaninovelPlayMaker/Runtime/Actions/GetLocalizationManagerStatus.cs
@@ -0,0 +1,37 @@
+﻿using HutongGames.PlayMaker;
+
+namespace Naninovel.PlayMaker
+{
+    [ActionCategory("Naninovel")]
+    public class GetLocalizationManagerStatus : FsmStateAction
+    {
+        [Tooltip("Whether the game is currently running in the default locale.")]
+        [UIHint(UIHint.Variable)]
+        public FsmBool UsingDefaulLocale;
+
+        [UIHint(UIHint.Variable)]
+        public FsmString DefaultLocale;
+
+        [UIHint(UIHint.Variable)]
+        public FsmString SelectedLocale;
+
+        public override void Reset ()
+        {
+            UsingDefaulLocale = null;
+            DefaultLocale = null;
+            SelectedLocale = null;
+        }
+
+        public override void OnEnter ()
+        {
+            var localizationManager = Engine.GetService<LocalizationManager>();
+            if (localizationManager is null) { Finish(); return; }
+
+            UsingDefaulLocale.Value = localizationManager.UsingDefaulLocale;
+            DefaultLocale.Value = localizationManager.DefaultLocale;
+            SelectedLocale.Value = localizationManager.SelectedLocale;
+
+            Finish();
+        }
+    }
+}
